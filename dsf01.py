07521b1e9ad76fc98fba74a80c72101ae47ec410
jadinhosuper <jadsoncarvalhoomilior1908@gmail.com>
2026-05-09 13:04:42 -0300

    Add files via upload

---
+++ dsf01.py
@@ -0,0 +1,9 @@
+nome = 'Jadson'
+input('Qual é o seu nome? ')
+print('Boas-vindas', nome)
+dia= 1
+mês= 9
+ano= 2008
+print('Meu nome é', nome, ' e minha data de nascimento é', dia, '/', mês, '/', ano)
+input(6+3)
+input('6'+'3')
---
+++ início.py
@@ -0,0 +1,15 @@
+print("Olá, mundo")
+print(7+4)
+print('7'+'4') 
+print('Olá', 5)
+print(1,2,3,4,5)
+nome = 'Jadim'
+idade = 17
+peso = 63.3
+nome = input('Qual é o seu nome? ') 
+idade = input('Qual é a sua idade? ')
+peso = input('Qual é o seu peso? ')
+print('Seu nome é', nome)
+print('Sua idade é', idade)
+print('Seu peso é', peso)
+print('Olá eu me chamo', nome, 'tenho', idade, 'anos e peso', peso, 'kg')
